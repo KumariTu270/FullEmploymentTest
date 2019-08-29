9eb0c54b0401cf040e00e6e2ec05168b6891a485
KumariTu270 <40356050+KumariTu270@users.noreply.github.com>
2019-08-29 11:44:23 +0530

    Third commit for learning

---
+++ git_Command.sh
@@ -0,0 +1,26 @@
+       git init
+  293  git pull https://github.com/KumariTu270/FullEmploymentTest.git
+  294  git status
+  295  git status
+  296  git add src*
+  297  git status
+  298  git commit -m "First commit"
+  299  git pull
+  300  git push
+  301  git remote add https://github.com/KumariTu270/FullEmploymentTest.git
+  302  git remote add https://github.com/KumariTu270/FullEmploymentTest
+  303  git remote add  origin https://github.com/KumariTu270/FullEmploymentTest.git
+  304  get remote -v
+  305  git remote -v
+  306  git pull
+  307  git status
+  308  git push
+  309  git push --set-upstream origin master
+  310  git status
+  311  git status
+  312  git add sr*
+  313  git status
+  314  git commit -m "Second commit"
+  315  git pull
+  316  git push
+  317  history
